8fa1bf58188250cb69f4f6eb64134e739014ecd1
Tia Vasquez <tiavasquez3@gmail.com>
2017-12-04 20:35:15 -0800

    few more tweaks

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,17 +15,32 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  afterConnection();
+  displayItems();
+  //prompt user asking for item# and quantity they want to purchase
+  //query by the item
+  queryByID(1); //just hard coding item 1 for testing
+  //if stock available is enough, do two things:
+  //1. calculate their total cost (price * quantity)
+  //2. update that item's quantity in the database (subtract quantity purchased from stock quantity)
+  
 });
 
-function afterConnection() {
+function displayItems() {
   console.log("These are the items available for purchase:")
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log("ITEM: " + res[i].item_id + " PRODUCT: " + res[i].product_name + " PRICE: $" + res[i].price);
+      console.log("ITEM: " + res[i].item_id + " | " + res[i].product_name + " | $" + res[i].price);
     }
     //console.log(res);
     connection.end();
+    
   });
+}
+
+function queryByID(selectedID) {
+  var query = connection.query("SELECT stock_quantity FROM products WHERE item_id=?", [selectedID], function(err, res) {
+    console.log("stock_quantity: " + res[0].stock_quantity);
+  });
+  
 }
